c35048fd57580b155d1d832a541b976d8962f5d0
Indiphile Ngqambuza <ingqambuza@gmail.com>
2024-10-06 20:40:09 +0200

    Move to EO-SANSA-VM

---
+++ desa-backend/services/ows_refactored/agriculture/ows_crop_mask_cfg.py
@@ -0,0 +1,216 @@
+import copy
+
+from ows_refactored.common.ows_reslim_cfg import reslim_alos_palsar
+
+bands_crop_mask = {"mask": [], "prob": [], "filtered": []}
+
+style_crop_mask_prob = {
+    "name": "prob",
+    "title": "Probability of cropping",
+    "abstract": "Probability of cropping",
+    "needed_bands": ["prob"],
+    "index_function": {
+        "function": "datacube_ows.band_utils.single_band",
+        "mapped_bands": True,
+        "kwargs": {
+            "band": "prob",
+        },
+    },
+    "color_ramp": [
+        {
+            "value": 0,
+            "color": "black",
+        },
+        {
+            "value": 1,
+            "color": "#010007",
+        },
+        {
+            "value": 10,
+            "color": "#170b3b",
+        },
+        {
+            "value": 20,
+            "color": "#410967",
+        },
+        {
+            "value": 30,
+            "color": "#6b176e",
+        },
+        {
+            "value": 40,
+            "color": "#952666",
+        },
+        {
+            "value": 50,
+            "color": "#bb3754",
+        },
+        {
+            "value": 60,
+            "color": "#dd5238",
+        },
+        {
+            "value": 70,
+            "color": "#f37719",
+        },
+        {
+            "value": 80,
+            "color": "#fba60b",
+        },
+        {
+            "value": 90,
+            "color": "#f5d948",
+        },
+        {
+            "value": 100,
+            "color": "#fcfea4",
+        },
+    ],
+    "range": [0, 100],
+    "legend": {
+        "begin": "0",
+        "end": "100",
+        "ticks_every": "20",
+    },
+}
+
+# Copy the probability styles and make the 0 values transparent
+style_crop_mask_prob_transparent = copy.deepcopy(style_crop_mask_prob)
+style_crop_mask_prob_transparent["color_ramp"][0]["alpha"] = 0.0
+
+
+style_crop_mask_green = {
+    "name": "green",
+    "title": "Cropped land",
+    "abstract": "Classified as crop",
+    "value_map": {
+        "mask": [
+            {
+                "title": "Cropped land",
+                "color": "#00FF00",  # (Or #FFFF00)
+                "values": [1],
+            }
+        ]
+    },
+}
+
+style_crop_mask_yellow = {
+    "name": "yellow",
+    "title": "Cropped land",
+    "abstract": "Classified as crop",
+    "value_map": {
+        "mask": [
+            {
+                "title": "Cropped land",
+                "color": "#FFFF00",  # (Or #FFFF00)
+                "values": [1],
+            }
+        ]
+    },
+}
+
+style_crop_mask_filtered_green = {
+    "name": "filtered_green",
+    "title": "Cropped land (object-filtered)",
+    "abstract": "Classified as crop and filtered",
+    "value_map": {
+        "filtered": [
+            {
+                "title": "Cropped land (object-filtered)",
+                "color": "#00FF00",  # (Or #FFFF00)
+                "values": [1],
+            }
+        ]
+    },
+}
+
+style_crop_mask_filtered_yellow = {
+    "name": "filtered_yellow",
+    "title": "Cropped land (object-filtered)",
+    "abstract": "Classified as crop and filtered",
+    "value_map": {
+        "filtered": [
+            {
+                "title": "Cropped land (object-filtered)",
+                "color": "#FFFF00",  # (Or #FFFF00)
+                "values": [1],
+            }
+        ]
+    },
+}
+
+style_crop_mask_reversed = {
+    "name": "reversed",
+    "title": "Reversed",
+    "abstract": "Classified as crop by the mask band",
+    "value_map": {
+        "mask": [
+            {
+                "title": "Cropped land",
+                "color": "#CCCCCC",  # (Or #FFFF00)
+                "values": [1],
+                "mask": True,  # transparent
+            },
+            {
+                "title": "No crop",
+                "color": "#CCCCCC",  # (Or #FFFF00)
+                "values": [0],
+            },
+        ]
+    },
+}
+
+layers = {
+    "title": "Cropland extent map (provisional)",
+    "abstract": """The Digital Earth Africa cropland extent service identifies areas of cropping in Africa, using Copernicus Sentinel-2 satellite imagery and machine learning techniques. The map indicates either the presence or absence of crop. An accurate, high-resolution, and regularly-updated cropland area map for the African continent is recognised as a useful tool in crop monitoring services. A precise and accurate cropland extent map indicating where cropland occurs serves as a basis for higher-level products, such as crop type and watering intensity.
+
+""",
+    "layers": [
+        {
+            "title": "Cropland extent 2019",
+            "name": "crop_mask",
+            "abstract": """
+Digital Earth Africa's cropland extent map (2019) shows the estimated location of croplands in Africa for the period January to December 2019. Cropland is defined as: "a piece of land of minimum 0.01 ha (a single 10m x 10m pixel) that is sowed/planted and harvest-able at least once within the 12 months after the sowing/planting date." This definition will exclude non-planted grazing lands and perennial crops which can be difficult for satellite imagery to differentiate from natural vegetation.
+
+This provisional cropland extent map has a resolution of 10m, and was built using Copernicus Sentinel-2 satellite images from 2019. The cropland extent map was produced using extensive training data from regions across Africa, coupled with a Random Forest machine learning model. The continental service contains maps built seperately for eight Agro-Ecological Zones (AEZs). For a detailed exploration of the methods used to produce the cropland extent map, read the Jupyter Notebooks in DE Africa’s crop-mask GitHub repository.
+
+Independent validations suggest this service has an overall accuracy ranging from 84 to 94 % across the AEZs. The algorithm tends to report more omission errors (labelling actual crops as non-crops) than commission errors (labelling non-crops as crops). Where commission errors occur they tend to be focussed around wetlands and seasonal grasslands (e.g. in the Serengeti) which spectrally resemble some kinds of cropping.
+
+The crop mask contains three measurements:
+- mask ("Cropped land"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is a pixel-based cropland extent map, meaning the map displays the raw output of the pixel-based Random Forest classification.
+- prob ("Probability of cropping"): This band displays the prediction probabilities for the 'crop' class during 2019. As this service used a random forest classifier, the prediction probabilities refer to the percentage of trees that voted for the random forest classification. For example, if the model had 200 decision trees in the random forest, and 150 of the trees voted 'crop', the prediction probability is 150 / 200 x 100 = 75 %. Thresholding this band at 50 % will produce a map identical to mask.
+- filtered ("Cropped land (object-filtered)"): This band displays cropped regions as a binary map. Values of 1 indicate the presence of crops, while a value of 0 indicates the absence of cropping. This band is an object-based cropland extent map where the mask band has filtered using an image segmentation algorithm. During this process, segments smaller than 1 Ha (100 10m x 10m pixels) are merged with neighbouring segments, resulting in a map where the smallest classified region is 1 Ha in size. The filtered dataset is provided as small commission errors are removed by this process, and the 'salt and pepper' effect typical of classifying pixels is diminished.
+
+More technical information about the cropland extent service can be found in the User Guide (https://docs.digitalearthafrica.org/en/latest/data_specs/Cropland_extent_specs.html)
+
+Cropland extent maps are a foundational, baseline layer in high-order crop health and crop productivity products which necessarily rely on knowing where cropping occurs before further analysis can take place.
+
+This service is accessible through OGC Web Service (https://ows.digitalearth.africa/), for analysis in DE Africa Sandbox JupyterLab (https://github.com/digitalearthafrica/deafrica-sandbox-notebooks/wiki) and for direct download click on a tile in the explorer page (https://explorer.digitalearth.africa/products/crop_mask_eastern/extents
+""",
+            "product_name": "crop_mask",
+            "time_resolution": "year",
+            "bands": bands_crop_mask,
+            "resource_limits": reslim_alos_palsar,
+            "image_processing": {
+                "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+                "always_fetch_bands": [],
+                "manual_merge": False,
+            },
+            "native_crs": "EPSG:6933",
+            "native_resolution": [10, -10],
+            "styling": {
+                "default_style": "green",
+                "styles": [
+                    style_crop_mask_green,
+                    style_crop_mask_filtered_yellow,
+                    style_crop_mask_prob,
+                ],
+            },
+        },
+    ],
+}
+
+
+print(style_crop_mask_prob)
+
+print(style_crop_mask_prob_transparent)
---
+++ desa-backend/services/ows_refactored/vegetation/ows_gmw_cfg.py
@@ -0,0 +1,47 @@
+from ows_refactored.common.ows_reslim_cfg import reslim_continental
+
+mangrove = {
+    "name": "mangrove",
+    "title": "Mangroves",
+    "abstract": "Global mangrove watch mangroves",
+    "value_map": {
+        "mangrove": [
+            {
+                "title": "Mangroves",
+                "color": "#00CED1",
+                "values": [1],
+            }
+        ]
+    }
+}
+
+
+layer = {
+    "title": "Global Mangrove Watch",
+    "name": "gmw",
+    "abstract": """
+The Global Mangrove Watch (GMW) aims to provide geospatial information about mangrove extent and changes to the Ramsar Convention, national wetland practitioners, decision makers and NGOs. It is part of the Ramsar Science and Technical Review Panel (STRP) work plan for 2016-2018 and a Pilot Project to the Ramsar Global Wetlands Observation System (GWOS), which is implemented under the GEO-Wetlands Initiative. The primary objective of the GMW has been to provide countries lacking a national mangrove monitoring system with first cut mangrove extent and change maps, to help safeguard against further mangrove forest loss and degradation.
+
+DE Africa provides access to GMW mangrove extent data over Africa. This dataset is available for the years 1996, 2007, 2008, 2009, 2010, 2015, and 2016. The extents have been rasterized to a spatial resolution of 0.0002 deg (~22 m).
+
+For more information, see https://data.unep-wcmc.org/datasets/45.
+""",
+    "product_name": "gmw",
+    "time_resolution": "year",
+    "bands": {
+        "mangrove": [],
+    },
+    "resource_limits": reslim_continental,
+    "dynamic": True,
+    "image_processing": {
+        "extent_mask_func": "datacube_ows.ogc_utils.mask_by_val",
+        "always_fetch_bands": [],
+        "manual_merge": False,
+    },
+    "native_crs": "EPSG:4326",
+    "native_resolution": [0.0002, -0.0002],
+    "styling": {
+        "default_style": "mangrove",
+        "styles": [mangrove],
+    }
+}
